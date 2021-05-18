78ef5a6d798e22aab898d1e96f87fb797c80993c
Harrison Kim <>
2021-05-18 17:01:52 +0800

    2 4 authentication log in complete

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,8 @@ const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+
   const onChange = (event) => {
     const {
       target: { name, value },
@@ -28,14 +30,16 @@ const Auth = () => {
           password
         );
       } else {
-        // log in
+        // Sign In
         data = await authService.signInWithEmailAndPassword(email, password);
       }
       console.log(data);
     } catch (error) {
-      console.log(error);
+      setError(error.message);
     }
   };
+
+  const toggleAccount = () => setNewAccount((prev) => !prev);
 
   return (
     <div>
@@ -56,8 +60,15 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Sign In"}
+        />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>
