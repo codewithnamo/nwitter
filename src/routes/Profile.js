39d9019b2e2d24c46d6f98b92d9d2df9323d6922
Harrison Kim <>
2021-05-18 18:37:28 +0800

    2.6 log out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,1 +1,16 @@
-export default () => <span>Profile</span>;
+import { authService } from "../fbase";
+import { useHistory } from "react-router-dom";
+
+export default () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
