1d9177076f43ade03fb3374801afa395aa979274
Harrison Kim <>
2021-05-19 16:00:38 +0800

    5 0 filter tweets by user id

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,12 +1,25 @@
-import { authService } from "../fbase";
+import { authService, dbService } from "../fbase";
 import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
 
-export default () => {
+export default ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+  const getMyTweets = async () => {
+    const tweets = await dbService
+      .collection("tweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+    console.log(tweets.docs.map((doc) => doc.data()));
+  };
+
+  useEffect(() => {
+    getMyTweets();
+  }, []);
 
   return (
     <>
