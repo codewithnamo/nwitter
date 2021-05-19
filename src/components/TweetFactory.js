bbdf30fa7f958fdd60c568508ce2357d095a3168
Harrison Kim <>
2021-05-19 23:16:21 +0800

    twitter NS 6 0 cleaning files

---
+++ src/components/TweetFactory.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { dbService, storageService } from "fbase";
+
+const TweetFactory = ({ userObj }) => {
+  const [tweet, setTweet] = useState("");
+  const [attachment, setAttachment] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    let attachmentUrl = "";
+    if (attachment !== "") {
+      const attachmentRef = storageService
+        .ref()
+        .child(`${userObj.uid}/${uuidv4()}`);
+      const response = await attachmentRef.putString(attachment, "data_url");
+      attachmentUrl = await response.ref.getDownloadURL();
+    }
+    const tweetObj = {
+      text: tweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+      attachmentUrl,
+    };
+    await dbService.collection("tweets").add(tweetObj);
+    setTweet(""); // to clear the input field
+    setAttachment(""); // clear attachment
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event; // remember read from right to left: inside of the event, go inside of the target, and get the files.
+    const theFile = files[0]; // we only have one file
+    const reader = new FileReader(); // this doesn't happen immediately. Need to wait a bit.
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+  const onClearAttachment = () => setAttachment(null);
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={tweet}
+        onChange={onChange}
+        type="text"
+        placeholder="What's on your mind?"
+        maxLength={120}
+      />
+      <input type="file" accept="image/*" onChange={onFileChange} />
+      <input type="submit" value="Tweet" />
+      {attachment && (
+        <div>
+          <img src={attachment} width="50px" height="50px" />
+          <button onClick={onClearAttachment}>Clear</button>
+        </div>
+      )}
+    </form>
+  );
+};
+
+export default TweetFactory;
